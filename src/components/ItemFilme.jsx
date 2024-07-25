22d99f7f5e13fc56c3e11ce5db21bfee04a58327
ProfaIngrid <ingrid.bconceicao@senacsp.edu.br>
2024-07-25 08:53:05 -0300

    aula 25/07

--- src/components/ItemFilme.jsx
+++ src/components/ItemFilme.jsx
@@ -2,16 +2,19 @@ import React, {useState} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar as faStarSolid } from '@fortawesome/free-solid-svg-icons';
 import { faStar as faStarRegular } from '@fortawesome/free-regular-svg-icons';
+import { Link } from 'react-router-dom';
 
 const ItemFilme = ({titulo, imagem, favorito}) => {
   const [fav, setFav] = useState();
 
-  const handleFavClick = () => {
+  const handleFavClick = (e) => {
+    e.preventDefault();
     const newFav = !fav;
     setFav(newFav);
   }
 
   return(
+    <Link to={'/exibetitulo'}>
       <div className='card-filme'>
         <div className='titulo-card'>
           {
@@ -23,6 +26,7 @@ const ItemFilme = ({titulo, imagem, favorito}) => {
         </div>
         <h3 className='titulo-font'>{titulo}</h3>
       </div>
+    </Link>
   )
 }
 
