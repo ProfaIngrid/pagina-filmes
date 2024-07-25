5c583a85ff4c56c7201b34044e2c3e47b5eddc66
ProfaIngrid <ingrid.bconceicao@senacsp.edu.br>
2024-07-25 08:31:54 -0300

    aula 24/07

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,25 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/Home';
 import ExibeTitulo from './components/ExibeTitulo';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 function App() {
+  const rotas = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home/>
+    },
+    {
+      path: '/exibetitulo',
+      element: <ExibeTitulo/>
+    }
+  ])
+
+
   return(
     // <Home/>
-    <ExibeTitulo/>
+    // <ExibeTitulo/>
+    <RouterProvider router={rotas}/>
   )
 }
 
--- src/components/ExibeTitulo.jsx
+++ src/components/ExibeTitulo.jsx
@@ -1,9 +1,31 @@
 import Header from "./Header";
+import Filme1 from '../img/filme1.jpg';
+import Accordion from 'react-bootstrap/Accordion';
 
 const ExibeTitulo = () => {
     return(
         <>
             <Header/>
+            <div className="exibe-titulo-body">
+                <img src={Filme1} alt="Abigail" />
+                <div className="exibe-titulo-desc">
+                    <h2>Abigail</h2>
+                    <Accordion defaultActiveKey="0">
+                        <Accordion.Item eventKey="0">
+                            <Accordion.Header>Sinopse</Accordion.Header>
+                            <Accordion.Body>
+                                teste de Sinopse
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey="1">
+                            <Accordion.Header>Trailer</Accordion.Header>
+                            <Accordion.Body>
+                                <iframe className="exibe-titulo-video" src="https://www.youtube.com/embed/4EEDJFCKryE" title="Abigail | Trailer 1 Oficial-  Dublado" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                            </Accordion.Body>
+                            </Accordion.Item>
+                    </Accordion>
+                </div>
+            </div>
         </>
     )
 }
